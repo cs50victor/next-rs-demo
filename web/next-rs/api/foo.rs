3b13ddc996dd9d272f5a532039018f389129c505
Victor A. <52110451+cs50victor@users.noreply.github.com>
2023-10-27 13:47:25 -0400

    init

---
+++ web/next-rs/api/foo.rs
@@ -0,0 +1,9 @@
+// Example api/foo.rs
+use vercel_runtime::{Body, Error, Request, Response, StatusCode};
+
+pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(Body::Text("Route is /api/foo".into()))?)
+}
